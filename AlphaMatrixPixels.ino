b3c1b0b78b06f993867e2a096bfde4f0b9a99476
heXor <hex1625616@gmail.com>
2025-12-22 14:42:03 +0300

    Refactor LED configuration in AlphaMatrixPixels.ino

--- AlphaMatrixPixels.ino
+++ AlphaMatrixPixels.ino
@@ -1,60 +1,10 @@
 #include <FastLED.h>
 #include "matrix_render.hpp"
+#include "led_config.hpp"
 
 constexpr uint8_t WIDTH = 8;
 constexpr uint8_t HEIGHT = 8;
 constexpr uint16_t NUM_LEDS = WIDTH * HEIGHT;
-
-// LED strip configuration via macros.
-//
-// Select a preset by number:
-//   #define LED_CFG 3
-//
-// The preset defines:
-// - LedChip (FastLED chipset type)
-// - cDataPin / cClockPin (if required)
-// - cLedRgbOrder (channel order)
-// - LED_INIT_MODE (which FastLED.addLeds overload to use)
-//
-// Notes:
-// - LED_INIT_MODE = 1: LED_TYPE + DATA_PIN + CLOCK_PIN + LED_RGB_TYPE
-// - LED_INIT_MODE = 2: LED_TYPE + DATA_PIN + LED_RGB_TYPE
-// - LED_INIT_MODE = 3: LED_TYPE + LED_RGB_TYPE
-
-
-#define LED_CFG 3
-
-
-  #if (LED_CFG == 1)
-    using LedChip = LPD6803;
-    constexpr uint8_t cDataPin = 11;
-    constexpr uint8_t cClockPin = 13;
-    constexpr EOrder cLedRgbOrder = RGB;
-    #define LED_INIT_MODE 1
-  #elif (LED_CFG == 2)
-    using LedChip = SM16716;
-    constexpr uint8_t cDataPin = 11;
-    constexpr uint8_t cClockPin = 13;
-    constexpr EOrder cLedRgbOrder = RGB;
-    #define LED_INIT_MODE 1
-  #elif (LED_CFG == 3)
-    using LedChip = WS2812;
-    constexpr uint8_t cDataPin = 6;
-    constexpr EOrder cLedRgbOrder = GRB;
-    #define LED_INIT_MODE 2
-  #elif (LED_CFG == 4)
-    using LedChip = LPD6803;
-    constexpr uint8_t cDataPin = D1;
-    constexpr uint8_t cClockPin = D2;
-    constexpr EOrder cLedRgbOrder = RGB;
-    #define LED_INIT_MODE 1
-  #elif (LED_CFG == 5)
-    using LedChip = APA102;
-    constexpr uint8_t cDataPin = 11;
-    constexpr uint8_t cClockPin = 13;
-    constexpr EOrder cLedRgbOrder = BRG;
-    #define LED_INIT_MODE 1
-  #endif
 
 CRGB leds[NUM_LEDS];
 
@@ -72,11 +22,11 @@ void setup() {
     constexpr uint16_t cLedCount = NUM_LEDS;
 
 #if (LED_INIT_MODE == 1)
-    FastLED.addLeds<LedChip, cDataPin, cClockPin, cLedRgbOrder>(leds, cLedCount);
+    FastLED.addLeds<LED_CHIPSET, cDataPin, cClockPin, cLedRgbOrder>(leds, cLedCount);
 #elif (LED_INIT_MODE == 2)
-    FastLED.addLeds<LedChip, cDataPin, cLedRgbOrder>(leds, cLedCount);
+    FastLED.addLeds<LED_CHIPSET, cDataPin, cLedRgbOrder>(leds, cLedCount);
 #elif (LED_INIT_MODE == 3)
-    FastLED.addLeds<LedChip, cLedRgbOrder>(leds, cLedCount);
+    FastLED.addLeds<LED_CHIPSET, cLedRgbOrder>(leds, cLedCount);
 #else
     #error "Invalid LED_INIT_MODE"
 #endif
---
+++ led_config.hpp
@@ -0,0 +1,70 @@
+// LED configuration presets for AlphaMatrixPixels Arduino sketch.
+// All comments in English (project convention).
+#pragma once
+
+#include <FastLED.h>
+
+// Select a preset by number:
+//   #define LED_CFG 3
+//
+// Each preset defines:
+// - LED_CHIPSET (FastLED chipset selector for addLeds templates)
+// - cDataPin / cClockPin (if required)
+// - cLedRgbOrder (channel order)
+// - LED_INIT_MODE (which FastLED.addLeds overload to use)
+//
+// Notes:
+// - LED_INIT_MODE = 1: LED_TYPE + DATA_PIN + CLOCK_PIN + LED_RGB_TYPE
+// - LED_INIT_MODE = 2: LED_TYPE + DATA_PIN + LED_RGB_TYPE
+// - LED_INIT_MODE = 3: LED_TYPE + LED_RGB_TYPE
+
+// If you prefer to keep this file untouched, override LED_CFG in your sketch
+// BEFORE including this header.
+#ifndef LED_CFG
+#define LED_CFG 3
+#endif
+
+#if (LED_CFG == 1)
+// Preset name: "елка"
+#define LED_CHIPSET LPD6803
+constexpr uint8_t cDataPin = 11;
+constexpr uint8_t cClockPin = 13;
+constexpr EOrder cLedRgbOrder = RGB;
+#define LED_INIT_MODE 1
+
+#elif (LED_CFG == 2)
+// Preset name: "матрица на окне"
+#define LED_CHIPSET SM16716
+constexpr uint8_t cDataPin = 11;
+constexpr uint8_t cClockPin = 13;
+constexpr EOrder cLedRgbOrder = RGB;
+#define LED_INIT_MODE 1
+
+#elif (LED_CFG == 3)
+// Preset name: "лента у брата"
+#define LED_CHIPSET WS2812
+constexpr uint8_t cDataPin = 6;
+constexpr EOrder cLedRgbOrder = GRB;
+#define LED_INIT_MODE 2
+
+#elif (LED_CFG == 4)
+// Preset name: "еще одна гирляна"
+#define LED_CHIPSET LPD6803
+constexpr uint8_t cDataPin = D1;
+constexpr uint8_t cClockPin = D2;
+constexpr EOrder cLedRgbOrder = RGB;
+#define LED_INIT_MODE 1
+
+#elif (LED_CFG == 5)
+// Preset name: "часы"
+#define LED_CHIPSET APA102
+constexpr uint8_t cDataPin = 11;
+constexpr uint8_t cClockPin = 13;
+constexpr EOrder cLedRgbOrder = BRG;
+#define LED_INIT_MODE 1
+
+#else
+#error "LED_CFG must be in range [1..5]"
+#endif
+
+
