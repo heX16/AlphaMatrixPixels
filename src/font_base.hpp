9ffa4b108f747a9a4a147af9b0af7c3b8ced5c8b
heXor <hex1625616@gmail.com>
2025-12-26 15:35:48 +0300

    Add getGlyphMatrix method to csFontBase for glyph rendering as csMatrixBoolean

--- src/font_base.hpp
+++ src/font_base.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <stdint.h>
+#include "matrix_types.hpp"
+#include "matrix_boolean.hpp"
 
 namespace amp {
 
@@ -19,6 +21,25 @@ public:
     virtual uint16_t count() const noexcept = 0;
 
     virtual uint32_t rowBits(uint16_t glyphIndex, uint16_t y) const noexcept = 0;
+
+    // Get glyph as csMatrixBoolean (bridge method).
+    // Returns nullptr if glyphIndex is out of range.
+    // Uses virtual methods width(), height(), rowBits() to build the matrix.
+    const csMatrixBoolean* getGlyphMatrix(uint16_t glyphIndex) const noexcept {
+        if (glyphIndex >= count()) {
+            return nullptr;
+        }
+        csMatrixBoolean * temp = 
+            new csMatrixBoolean(to_size(width()), to_size(height()));
+        for (uint16_t y = 0; y < height(); ++y) {
+            const uint32_t row = rowBits(glyphIndex, y);
+            for (uint16_t x = 0; x < width(); ++x) {
+                const bool bit = (row & (0x80000000U >> x)) != 0;
+                temp->setPixel(x, y, bit);
+            }
+        }
+        return temp;
+    }
 };
 
 } // namespace amp
