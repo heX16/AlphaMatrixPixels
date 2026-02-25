be38b49fd62134f0efe4bf445745430868632b8c
heXor <hex1625616@gmail.com>
2026-02-26 01:29:40 +0300

    Enhance random number generation in rand_gen.hpp
    - Added new methods for generating 16-bit random numbers: rand16() and rand16(lim).

--- src/rand_gen.hpp
+++ src/rand_gen.hpp
@@ -42,19 +42,56 @@ public:
     /// @param lim the upper bound (not inclusive) for the result
     uint8_t rand(uint8_t lim)
     {
-        uint8_t r = this->rand();
+        uint16_t r = this->rand();
         r = (r*lim) >> 8;
         return r;
     }
 
     /// Generate an 8-bit random number in the given range
+    /// @param min the lower bound (inclusive) for the random number
+    /// @param max the upper bound (inclusive) for the random number
+    uint8_t randRange(const uint8_t min, const uint8_t max)
+    {
+        if (min > max) {
+            return min; // return min as it is the only valid value (invalid range)
+        }
+        const uint8_t delta = max - min;
+        if (delta == 255) {
+            return this->rand();
+        }
+        uint8_t r = this->rand(delta + 1) + min;
+        return r;
+    }
+
+    /// Generate a 16-bit random number
+    uint16_t rand16()
+    {
+        rand_seed = (this->rand_seed * RAND16_2053) + RAND16_13849;
+        return this->rand_seed;
+    }
+
+    /// Generate a 16-bit random number between 0 and lim
+    /// @param lim the upper bound (not inclusive) for the result
+    uint16_t rand16(const uint16_t lim)
+    {
+        uint32_t r = this->rand16();
+        r = (r * lim) >> 16;
+        return r;
+    }
+
+    /// Generate a 16-bit random number in the given range
     /// @param min the lower bound for the random number
     /// @param max upper bound (inclusive) for the random number
-    uint8_t randRange(uint8_t min, uint8_t max)
+    uint16_t randRange16(const uint16_t min, const uint16_t max)
     {
-        uint8_t delta = max - min + 1;
-        uint8_t r = this->rand(delta) + min;
-        return r;
+        if (min > max) {
+            return min; // return min as it is the only valid value (invalid range)
+        }
+        const uint16_t delta = max - min;
+        if (delta == 65535) {
+            return rand16();
+        }
+        return this->rand16(delta + 1) + min;
     }
 
     /// Add entropy into the random number generator
